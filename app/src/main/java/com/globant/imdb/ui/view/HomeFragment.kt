447c2202a8a229c340001f413d892501cb46c641
Benjamin Silva <benjamin.silva@globant.com>
2023-10-27 22:09:03 -0500

    feature(Home): Add refresh swipe up layout

--- app/src/main/java/com/globant/imdb/ui/view/HomeFragment.kt
+++ app/src/main/java/com/globant/imdb/ui/view/HomeFragment.kt
@@ -2,17 +2,17 @@ package com.globant.imdb.ui.view
 
 import android.app.AlertDialog
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.globant.imdb.R
 import com.globant.imdb.core.RetrofitHelper
 import com.globant.imdb.databinding.FragmentHomeBinding
 import com.globant.imdb.ui.viewmodel.MovieViewModel
 import com.squareup.picasso.Picasso
+
 
 class HomeFragment : Fragment() {
 
@@ -36,7 +36,21 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //Setup
+        setupButtons()
+        setupLiveData()
+
+        movieViewModel.onCreate()
+    }
+
+    private fun setupButtons(){
+        binding.refreshLayout.setOnRefreshListener(::refresh)
+    }
+
+    private fun setupLiveData(){
         movieViewModel.mainMovie.observe(viewLifecycleOwner) { currentMovie ->
+            binding.refreshLayout.isRefreshing = false
             with(binding.mainTrailerContainer) {
                 trailerName.text = currentMovie.title
                 val imageUrl = RetrofitHelper.imageUrl + currentMovie.backdropPath
@@ -47,6 +61,9 @@ class HomeFragment : Fragment() {
                     .into(trailerImageView)
             }
         }
+    }
+
+    private fun refresh(){
         movieViewModel.onCreate()
     }
 
