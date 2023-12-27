7adf328be0fc3fe13086ddcd89563f6b0a19076e
Benjamin Silva <benjamin.silva@globant.com>
2023-12-27 13:12:51 -0500

    chore(ui): Create module for common ui components

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,10 +15,11 @@ dependencyResolutionManagement {
 
 rootProject.name = "IMDb"
 include(":app")
-include(":data:auth")
-include(":data:movies")
-include(":core:common")
-include(":domain:auth")
-include(":domain:movies")
-include(":feature:auth")
 include(":feature:home")
+include(":feature:auth")
+include(":domain:movies")
+include(":domain:auth")
+include(":data:movies")
+include(":data:auth")
+include(":core:common")
+include(":core:ui")
